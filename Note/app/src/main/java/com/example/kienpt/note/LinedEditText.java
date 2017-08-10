b42fff1be85b4c0a7f2c945d09c65f90e92f5fbe
Kaphetz <kienpt91@gmail.com>
2017-08-10 22:51:29 +0700

    Fix bug

---
+++ Note/app/src/main/java/com/example/kienpt/note/LinedEditText.java
@@ -0,0 +1,55 @@
+package com.example.kienpt.note;
+
+/**
+ * Created by Mohammad.N on 28/08/2016.
+ */
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class LinedEditText extends EditText
+{
+    private Rect mRect;
+    private Paint mPaint;
+
+    public LinedEditText(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        mRect = new Rect();
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(0xFF000000);
+    }
+
+    /**
+     * This is called to draw the LinedEditText object
+     * @param canvas The canvas on which the background is drawn.
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        int height = getHeight();
+        int line_height = getLineHeight();
+
+        int count = height / line_height;
+
+        if (getLineCount() > count)
+            count = getLineCount();
+
+        Rect r = mRect;
+        Paint paint = mPaint;
+        int baseline = getLineBounds(0, r);
+
+        for (int i = 0; i < count; i++) {
+
+            canvas.drawLine(r.left, baseline + 1, r.right, baseline + 1, paint);
+            baseline += getLineHeight();
+
+            super.onDraw(canvas);
+        }
+    }
+}
